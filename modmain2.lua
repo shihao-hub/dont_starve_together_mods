0533990bb7bfd6b0b1e3772ce0677338704a4fc0
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-12-27 14:32:58 +0800

    Create modmain2.lua

---
+++ modmain2.lua
@@ -0,0 +1,13 @@
+---
+--- DateTime: 2024/12/5 16:35
+---
+
+local shihao = require("moreitems.main")
+local dst = shihao.dst
+
+local dst_utils = dst.DSTUtils(env)
+
+env.modimport("mod_config.lua")
+
+
+-- 2024-12-05-17:30 止于此，今日只是将模组整理了一番，本周暂且如此吧。接下来两天优先 java 和 js，但是这两天关于 Lua 可以总结一下。
