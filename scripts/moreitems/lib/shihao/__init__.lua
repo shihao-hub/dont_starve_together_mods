72c481924c24be7b8071054e010a2ec528993655
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-01-06 20:30:09 +0800

    Update __init__.lua

--- scripts/moreitems/lib/shihao/__init__.lua
+++ scripts/moreitems/lib/shihao/__init__.lua
@@ -1,8 +1,10 @@
 -- 将 __init__.lua 类比 Python 的 __init__.py 即可，外界需要导入库的话都从这里取
 
 return {
+    class = require("moreitems.lib.shihao.class.__init__"),
+    module = require("moreitems.lib.shihao.module.__init__"),
+    stl = require("moreitems.lib.shihao.stl.__init__"),
+
     base = require("moreitems.lib.shihao.base"),
     utils = require("moreitems.lib.shihao.utils"),
-    log = require("moreitems.lib.shihao.module.log"),
-    checker = require("moreitems.lib.shihao.module.checker"),
 }
