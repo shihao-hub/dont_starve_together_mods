f21b99595b731a672c0b77bef6f6abf67217c4e1
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-01-07 19:01:50 +0800

    Create __init__.lua

---
+++ scripts/moreitems/lib/shihao/module/stl/__init__.lua
@@ -0,0 +1,5 @@
+return {
+    debug = require("moreitems.lib.shihao.module.stl.debug"),
+    string = require("moreitems.lib.shihao.module.stl.string"),
+    table = require("moreitems.lib.shihao.module.stl.table"),
+}
