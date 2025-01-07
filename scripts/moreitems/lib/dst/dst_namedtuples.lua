569b78022be349fb65ab810e3ae657de9c88d0d2
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-01-07 18:57:41 +0800

    Create dst_namedtuples.lua

---
+++ scripts/moreitems/lib/dst/dst_namedtuples.lua
@@ -0,0 +1,12 @@
+---
+--- DateTime: 2025/1/7 18:21
+---
+
+local NamedTuple = require("moreitems.lib.shihao.class.NamedTuple")
+
+-- TODO: 应该是 namedtuple 函数返回一个类，然后传入 fields 用于实例化...
+--local WidgetSetupParamsNT = NamedTuple({})
+
+return {
+
+}
