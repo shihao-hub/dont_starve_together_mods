7aa500a118830e811d4aea828d1156e52fa00fe7
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-01-07 18:57:02 +0800

    Update __init__.lua

--- scripts/moreitems/lib/dst/__init__.lua
+++ scripts/moreitems/lib/dst/__init__.lua
@@ -4,10 +4,13 @@
 
 -- class 将一律大写
 
+-- 在我看来，dst 根本不需要 module 目录
+
 return {
     class = require("moreitems.lib.dst.class.__init__"),
-    module = require("moreitems.lib.dst.module.__init__"),
+    --module = require("moreitems.lib.dst.module.__init__"),
 
+    dst_namedtuples = require("moreitems.lib.dst.dst_namedtuples"),
     dst_utils = require("moreitems.lib.dst.dst_utils"),
     dst_service = require("moreitems.lib.dst.dst_service"),
 }
