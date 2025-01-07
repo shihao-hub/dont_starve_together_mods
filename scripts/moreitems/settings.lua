00f8d8a4cb8f7e85fb9e39d5064bef844c78b8ad
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-01-07 18:52:48 +0800

    Update settings.lua

--- scripts/moreitems/settings.lua
+++ scripts/moreitems/settings.lua
@@ -1,5 +1,11 @@
 local module = {}
 
+-- 正常开发的项目在自动化部署的时候可以用脚本替换 DEBUG -> false
+module.DEBUG = true
+
+-- 启动测试用例的执行，默认应该是 false，通过命令修改其为 true，测试完毕再修改为 false（这意味着测试用例的运行环境必须是个沙盒，至少让崩溃后可以进入 __exit__ 逻辑）
+module.TEST_ENABLED = true
+
 -- 如何获取？好像有点困难
 module.BASE_DIR = ""
 
