0e8834b99cf5aab105119e856c0da6f5f6fb1e35
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-01-27 15:48:37 +0800

    Update base.lua

--- scripts/moreitems/lib/shihao/base.lua
+++ scripts/moreitems/lib/shihao/base.lua
@@ -25,6 +25,16 @@ local function _string_format2(template, context)
     return lustache.renderer:render(template, context)
 end
 
+---将形如 {{ a }} {{ b }} 等格式的字符串全部替换为 tostring(a) 和 tostring(b)
+function module.string_format(template, context)
+    return _string_format(template, context)
+end
+
+---python f-string
+function module.f_string(str, context)
+    return _string_format(str, context)
+end
+
 -- 这个方法应该是回答了之前的疑问：如果 module.fn1 需要用到 module.fn2，如何避免一不小心写出循环依赖的情况？这种方式应该可以
 local function _bool(val)
     if val == nil or val == false then
@@ -33,8 +43,14 @@ local function _bool(val)
     return true
 end
 
+function module.bool(val)
+    return _bool(val)
+end
+
+
+
 -- 大致简单模仿了 js 的参数归一化
-local function _normalize_parameter_of_if_then_else(arg)
+local function _normalize_parameter(arg)
     -- JS:
     --  如果是函数，则设置返回的 get = arg, set = function() error("xxx was assigned to but it has no setter.") end
     --  如果不是函数，则设置返回的 get = arg.get, set = arg.set
@@ -42,6 +58,7 @@ local function _normalize_parameter_of_if_then_else(arg)
     -- Question: 那 Lua 中如何是好？好像几乎没有参考价值。。。
     --  按照目前的实现的话，其实设置一个 class，然后返回值是这个 class，挺好的，至少用户知道咋用就行了...
 
+    -- normalize，这也算归一化。
     if type(arg) ~= "function" then
         local old_arg = arg
         arg = function()
@@ -49,6 +66,7 @@ local function _normalize_parameter_of_if_then_else(arg)
         end
     end
 
+    -- normalize
     local obj = {}
 
     obj._value = nil
@@ -67,27 +85,14 @@ local function _normalize_parameter_of_if_then_else(arg)
     return obj
 end
 
----将形如 {{ a }} {{ b }} 等格式的字符串全部替换为 tostring(a) 和 tostring(b)
-function module.string_format(template, context)
-    return _string_format(template, context)
-end
-
----python f-string
-function module.f_string(str, context)
-    return _string_format(str, context)
-end
-
-function module.bool(val)
-    return _bool(val)
-end
-
-function module.if_then_else(condition, expression_or_lazy_fn1, expression_or_lazy_fn2)
+function module.if_then_else(condition, true_branch, false_branch)
     -- 新想法：推荐始终使用 lazy_fn
-    assert(type(expression_or_lazy_fn1) == "function")
-    assert(type(expression_or_lazy_fn2) == "function")
+    assert(type(true_branch) == "function")
+    assert(type(false_branch) == "function")
 
-    local arg2 = _normalize_parameter_of_if_then_else(expression_or_lazy_fn1)
-    local arg3 = _normalize_parameter_of_if_then_else(expression_or_lazy_fn2)
+    -- 注意，此处的代码写法是没意义的，但是主要是简单模仿 js 视频中的关于参数归一化的技巧。（只要会嘎嘎叫那就是鸭子）
+    local arg2 = _normalize_parameter(true_branch)
+    local arg3 = _normalize_parameter(false_branch)
 
     -- 不允许发生误解，一律转为 boolean 类型
     if _bool(condition) then
@@ -130,8 +135,69 @@ function module.switch(condition)
     end
 end
 
-function module.get_args(...)
+local function _get_args(...)
     return { n = select("#", ...), ... }
+end
+
+function module.get_args(...)
+    return _get_args(...)
+end
+
+local function _ipairs_has_hole(t)
+    local len = t.n
+    if len == nil then
+        --error("The `t` don't have the key `n`.", 2)
+        local n = #t
+        local maxn = table.maxn(t)
+        assert(n == maxn)
+        len = math.max(n, maxn)
+    end
+    local index = 1
+    return function()
+        if index > len then
+            return nil
+        end
+        index = index + 1
+        return index - 1, t[index - 1]
+    end
+end
+
+function module.ipairs_has_hole(t)
+    return _ipairs_has_hole(t)
+end
+
+---依次检查每个键的存在性。如果在某个阶段找不到对应的键，函数将返回 `nil`，而不会抛出错误。
+---
+---可变参数为空时，返回 chain 链末尾值。可变参数不为空时，调用函数。
+---@param root table
+---@param chain string
+function module.optional_chain(root, chain, ...)
+    local res
+    local args = _get_args(...)
+
+    local chain_elems = {}
+    local _, n = string.gsub(chain, "([^.]+)", function(capture)
+        table.insert(chain_elems, capture)
+    end)
+    chain_elems.n = n
+
+    local elem = root
+    for i = 2, chain_elems.n do
+        if elem == nil then
+            return nil
+        end
+        elem = elem[chain_elems[i]]
+    end
+
+    if args.n == 0 then
+        res = elem
+    else
+        if type(elem) ~= "function" then
+            error("The last value of `" .. chain .. "`" .. " is unexpected ==> expected: a function type, but was: " .. type(elem) .. ".", 2)
+        end
+        res = elem(unpack(args, 1, args.n))
+    end
+    return res
 end
 
 ---Usage:
@@ -205,6 +271,39 @@ if select("#", ...) == 0 then
         }
         assertion.assert_true(res == 123)
     end, function(msg) io.stderr:write(msg, "\n") end)
+
+    --[[ optional_chain ]]
+    print(module.optional_chain({}, "inst"))
+
+    local function foreachWithNil(array, callback)
+        -- 遍历整个数组，包括 nil 值
+        for i = 1, #array do
+            callback(array[i], i) -- 直接调用回调，允许为 nil
+        end
+    end
+
+    local myArray = { 1, 2, nil, 4, nil, 6 }
+    print(#myArray) -- # 居然和 table.maxn 一样？不对啊，# 不是和 ipairs 一样吗
+    print(table.maxn(myArray))
+
+    foreachWithNil(myArray, function(value, index)
+        if value == nil then
+            print("Element at index " .. index .. " is nil")
+        else
+            print("Element at index " .. index .. " is " .. value)
+        end
+    end)
+    print("------")
+
+    --[[ ipairs_has_hole ]]
+    local t = { 1, 2, nil, 4, 5, nil, 7, 8 }
+    for i, v in module.ipairs_has_hole(t) do
+        print(i, v)
+    end
+    print("------")
+    for i, v in ipairs(t) do
+        print(i, v)
+    end
 end
 
 return module
